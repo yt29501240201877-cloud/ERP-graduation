65cdafad68e2983f1f254f14241e7d142e2ff762
yahia-elshamy <y.ahmed0112000@gmail.com>
2026-08-09 18:36:17 +0300

    update the usersRoutes file to implement the routes for the notification

--- BackEnd/Routes/usersRoutes.js
+++ BackEnd/Routes/usersRoutes.js
@@ -7,12 +7,17 @@ const authorize = require("../Middlewares/authorize");
 
 const {login, register, logout} = require("../Controllers/authController");
 const {getallusers, updaterole, updateUser, deleteUser} = require("../Controllers/userController")
+const {getMyNotifications, markAsRead, deleteNotification, markAllAsRead} = require("../Controllers/notificationController");
 
 const {uploadprofileImage} = require("../Middlewares/UploadImage");
 
 router.post("/register", authenticate, authorize("Admin"), uploadprofileImage, register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/my-notifications", authenticate, getMyNotifications);
+router.patch("/notifications/read-all", authenticate, markAllAsRead);
+router.patch("/notifications/:id/read", authenticate, markAsRead);
+router.delete("/notifications/:id", authenticate, deleteNotification);
 
 router.get("/users", authenticate, authorize("Admin"), getallusers);
 router.put("/:id", authenticate, authorize("Admin"), updateUser)
